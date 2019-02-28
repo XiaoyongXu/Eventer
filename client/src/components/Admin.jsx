a6b2c01cfc7ec04cadaaeb042cd5a7170ef621e9
XiaoyongXu <xxy1994212@gmail.com>
2019-02-28 01:40:13 +0000

    add demo of using api fetching data from server

--- client/src/components/Admin.jsx
+++ client/src/components/Admin.jsx
@@ -1,23 +1,41 @@
-import React from 'react';
+import React,{Component} from 'react';
 import { Form, Button } from 'react-bootstrap';
 
-export default function Admin(props){
 
-  return (
-    <div style={{ width: '50%', marginLeft: "25%", marginTop: "10%" }}>
-      <Form>
-        <Form.Group controlId="exampleForm.ControlInput1">
-          <Form.Label>Activity name</Form.Label>
-              <Form.Control type="email" placeholder="title" />
-        </Form.Group>
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-              <Form.Label>Description</Form.Label>
-              <Form.Control as="textarea" rows="3" />
-            </Form.Group>
-        <Button variant="primary" type="submit">
-            Submit
-          </Button>
-      </Form >
-    </div>
-  );
+class Admin extends Component {
+  constructor(props){
+    super(props);
+  }
+  DemoShow(e) {
+    e.preventDefault();
+    //call the fetch function
+    fetch('http://localhost:5000/demo')
+      .then(res => res.json())//response type
+      .then
+      (
+        data => console.log(data)
+      ); //log the data;
+  }
+  render() {
+    return (
+      <div style={{ width: '50%', marginLeft: "25%", marginTop: "10%" }}>
+        <Form>
+          <Form.Group controlId="exampleForm.ControlInput1">
+            <Form.Label>Activity name</Form.Label>
+            <Form.Control type="text" placeholder="title" />
+          </Form.Group>
+          <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Description</Form.Label>
+            <Form.Control as="textarea" rows="3" />
+          </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+            <Button onClick={this.DemoShow}>Demo</Button>
+        </Form>
+      </div>
+    )
+  }
 }
+
+export default Admin;
--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
@@ -13,6 +19,13 @@ app.post('/api/world', (req, res) => {
   res.send(
     `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
+});
+app.get("/demo",(req,res)=>{
+  var data = [
+    {id:1, name:"Tony", job:"Project Manager"},
+    {id: 2, name: "Rohit", job: "Mentor" },
+  ];
+  res.send({data: data});
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
