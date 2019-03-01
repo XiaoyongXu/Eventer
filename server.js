399a634b71932a62020955d055def7115c465861
Terence Yau <tyauzer@hotmail.com>
2019-03-01 18:24:13 +0000

    updated server.js

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+const ENV = process.env.ENV || "development";
+const knexConfig = require("./knexfile");
+const knex = require("knex")(knexConfig[ENV]);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -16,17 +20,29 @@ app.get('/api/hello', (req, res) => {
 });
 app.post('/api/world', (req, res) => {
   console.log(req.body);
-  res.json({
-    data: req.body,
-  }
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
 });
-app.get("/demo",(req,res)=>{
+app.get("/demo", (req, res) => {
   var data = [
-    {id:1, name:"Tony", job:"Project Manager"},
-    {id: 2, name: "Rohit", job: "Mentor" },
+    { id: 1, name: "Tony", job: "Project Manager" },
+    { id: 2, name: "Rohit", job: "Mentor" },
   ];
-  res.send({data: data});
+  res.send({ data: data });
 });
+
+app.get("/test", (req, res) => {
+  knex('users')
+    .select('*')
+    .then(function (rows) {
+      rows.forEach(row => {
+        console.log(row)
+      })
+      res.redirect("/demo");
+    })
+})
+
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
