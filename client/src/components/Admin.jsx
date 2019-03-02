824f8ffca1580a02a5ab589f9e1179593b65b6dd
Terence Yau <tyauzer@hotmail.com>
2019-03-02 01:50:57 +0000

    added end date for events

--- client/src/components/Admin.jsx
+++ client/src/components/Admin.jsx
@@ -1,17 +1,27 @@
-import React,{Component} from 'react';
-import { Form, Button } from 'react-bootstrap';
-import axios from 'axios';
-
+import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
+import axios from "axios";
 
 class Admin extends Component {
   constructor(props) {
     super(props);
-    this.state = { title: '', description:'' };
+    this.state = { title: "", description: "", start_date: "", end_date: "" };
 
+    this.handleStartDate = this.handleStartDate.bind(this);
+    this.handleEndDate = this.handleEndDate.bind(this);
     this.handleTitleChange = this.handleTitleChange.bind(this);
     this.handleDescripChange = this.handleDescripChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  handleStartDate(event) {
+    this.setState({ start_date: event.target.value });
+  }
+
+  handleEndDate(event) {
+    this.setState({ end_date: event.target.value });
+  }
+
   handleTitleChange(event) {
     this.setState({ title: event.target.value });
   }
@@ -22,33 +32,60 @@ class Admin extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    axios.post('http://localhost:5000/admin', {
-      Title: this.state.title,
-      Description: this.state.description
-    })//response type
-      .then(
-        res => console.log(res.data)
-      );
+    axios
+      .post("http://localhost:5000/admin", {
+        title: this.state.title,
+        description: this.state.description,
+        start_date: this.state.start_date,
+        end_date: this.state.end_date
+      }) //response type
+      .then(res => console.log(res.data));
   }
 
   render() {
     return (
-      <div style={{ width: '50%', marginLeft: "25%", marginTop: "10%" }}>
+      <div style={{ width: "50%", marginLeft: "25%", marginTop: "10%" }}>
         <Form onSubmit={this.handleSubmit}>
+          <Form.Group controlId="exampleForm.ControlInput2">
+            <Form.Label>Start Date</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="YYYY-MM-DD"
+              onChange={this.handleStartDate}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="exampleForm.ControlInput3">
+            <Form.Label>End Date</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="YYYY-MM-DD"
+              onChange={this.handleEndDate}
+            />
+          </Form.Group>
+
           <Form.Group controlId="exampleForm.ControlInput1">
             <Form.Label>Activity name</Form.Label>
-            <Form.Control type="text" placeholder="title" onChange={this.handleTitleChange}/>
+            <Form.Control
+              type="text"
+              placeholder="title"
+              onChange={this.handleTitleChange}
+            />
           </Form.Group>
           <Form.Group controlId="exampleForm.ControlTextarea1">
             <Form.Label>Description</Form.Label>
-            <Form.Control as="textarea" rows="3" onChange={this.handleDescripChange}/>
+            <Form.Control
+              as="textarea"
+              rows="3"
+              onChange={this.handleDescripChange}
+            />
           </Form.Group>
-            <Button variant="primary" type="submit" >
-              Submit
-            </Button>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
         </Form>
       </div>
-    )
+    );
   }
 }
 
