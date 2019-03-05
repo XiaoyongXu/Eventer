1c22a86e3214e0fb324237fcd000038bceb73f61
Terence Yau <tyauzer@hotmail.com>
2019-03-05 19:10:20 +0000

    discussion fortmatting

--- client/src/components/DiscussionItem.jsx
+++ client/src/components/DiscussionItem.jsx
@@ -24,34 +24,29 @@ console.log(props);
     // </Card>
 
     // <Card border="dark" style={{ width: '18rem' }}>
-    //   <Card.Header>Header</Card.Header>
+    //   <Card.Header>Discussion</Card.Header>
     //   <Card.Body>
-    //     <Card.Title>Post subject</Card.Title>
+    //     <Card.Title>{props.message.contents}</Card.Title>
     //     <Card.Text>
-    //       Some quick example text to build on the card title and make up the bulk
-    //       of the card's content.
+    //       User details
     //   </Card.Text>
     //   </Card.Body>
     // </Card>
-    // <br />
 
-    <div className="meetup-record-holder">
-      <div className="container full_row">
-        {this.state.users.map(function (single_user1, i) {
-          var single_user = single_user1._source;
-          return (
-            <User
-              key={i}
-              index={i}
-              first_name={props.users.first_name}
-              last_name={props.users.last_name}
-              message={props}
-              event_url={single_user.event.event_url}
-            ></User>
-          );
-        })}
-      </div>
-    </div>
-
+    <Card>
+      <Card.Header>Discussion</Card.Header>
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p>
+            {' '}
+            {props.message.contents}
+            {' '}
+          </p>
+          <footer className="blockquote-footer">
+            <cite title="Source Title">User details</cite>
+          </footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
   )
 }
