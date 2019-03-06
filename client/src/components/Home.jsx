48acbec92e2d05c2f6b59207503bdd2839d25807
Lwong01 <leon.lo.wong@gmail.com>
2019-03-06 20:30:09 +0000

    Calendar and filter without database

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,55 +1,67 @@
 import React, { Component } from 'react';
 import Calendar from 'react-calendar';
 
+let events = [
+  {
+    id: 1,
+    name: "Stanley Park",
+    date: "Mon Mar 04 2019"
+  },
+  {
+    id: 2,
+    name: "Richmond Center",
+    date: "Wed Mar 06 2019"
+  },
+  {
+    id: 3,
+    name: "Granville Island",
+    date: "Wed Mar 06 2019"
+  },
+  {
+    id: 4,
+    name: "Burnaby",
+    date: "Sun Mar 10 2019"
+  }
+]
+
 
 class MyApp extends Component {
   constructor(props){
-    super(props){
+    super(props)
       this.state = {
         date: new Date(),
-        events:events
+        events:events,
+        term: '',
+
       }
 
-    }
+
   }
 
+  onChange = date => {
+    console.log("this is the date", date, typeof(date));
+    let dateOnly = date.toDateString();
+    console.log("This is the date without time", dateOnly);
+    this.setState({ dateOnly })
 
-  onChange = date => this.setState({ date })
-
+  }
   render() {
-    let events = [
-      {
-        id: 1,
-        name: "Stanley Park",
-        date: 20190304
-      },
-      {
-        id: 2,
-        name: "Richmond Center",
-        date: 20190306
-      },
-      {
-        id: 3,
-        name: "Granville Island",
-        date: 20190306
-      },
-      {
-        id: 4,
-        name: "Burnaby",
-        date: 20190310
-      }
-    ]
+    const x = events.filter(element => {
+      return element.date === this.state.dateOnly;
+    });
+    console.log("This is the YELL value", x)
     return (
       <div>
-      {
-        events.map(activity =>
+        {
+          x.map(activity =>
             <div key={activity.id}>
-            <h1>{activity.name}</h1>
-            <h1>{activity.date}</h1>
+              <h1>{activity.name}</h1>
+              <h1>{activity.date}</h1>
             </div>
 
-        )
-      }
+          )
+        }
+        <h1>{this.state.test}</h1>
         <Calendar
           onChange={this.onChange}
           value={this.state.date}
@@ -58,13 +70,5 @@ class MyApp extends Component {
     );
   }
 }
-
-// class Home extends Component {
-//   render() {
-//     return (
-//      <div>Home</div>
-//     );
-//   }
-// }
 
 export default MyApp;
