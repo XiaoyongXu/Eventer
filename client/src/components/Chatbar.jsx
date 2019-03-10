96d4de2d967099debcca5da0ccf637b6958cf1a9
Terence Yau <tyauzer@hotmail.com>
2019-03-10 23:19:59 +0000

    styled discussion page

--- client/src/components/Chatbar.jsx
+++ client/src/components/Chatbar.jsx
@@ -4,12 +4,14 @@ import React from 'react';
 export default function Chatbar(props) {
 
   return (
-      <input
-        className="chatbar-message"
-        placeholder="Type a message and hit ENTER"
+
+      <input className="chatbar-message"
+
+        placeholder="Type a message and press ENTER"
         onChange={props.handleChange}
         onKeyUp={props.handleEnterPress}
         />
+
   )
 
 }
